77bdc9160eb57c8a79dee081b3357065c51aadac
stribyw <stribyw@oregonstate.edu>
2018-06-06 10:01:27 -0700

    Corrected home.php so that the styling went back to the previous edition.

--- CS340/MTG_site/home.php
+++ CS340/MTG_site/home.php
@@ -7,41 +7,19 @@ include("header.php");
 include("db_connect.php");
 // session_start();
 
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-// 	$User_ID = mysqli_real_escape_string($conn, $_POST['User_ID']);
-// 	$password = mysqli_real_escape_string($conn, $_POST['password']);
-
-// 				// See if username is in the table
-// 	$queryIn = "SELECT * FROM User where User_ID='$User_ID'";
-// 	$resultIn = mysqli_query($conn, $queryIn);
-// 	if ($row = mysqli_fetch_assoc($resultIn)) {
-// 		$salt = $row['salt'];
-// 		$saltedPassword = MD5($password.$salt);
-// 		if ($saltedPassword == $row['password']){
-// 			echo "Log in successful!";
-// 		} else {
-// 			$rowpassword = $row['password'];
-// 			echo "ERROR, wrong username or password.";
-// 		}
-// 	} else {
-// 		echo "ERROR, wrong username or password.";
-// 	}
-// }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 		// Escape user inputs for security
 	$username  = mysqli_real_escape_string($conn, $_POST['username']);
 	$password  = mysqli_real_escape_string($conn, $_POST['password']);
-	
+
 	$queryIn = "SELECT * FROM User WHERE User_ID='$username'";
 
 	$resultIn = mysqli_query($conn, $queryIn);
 		if (!mysqli_num_rows($resultIn)) { // See if username is already in the table
 			// $msg ="<h2>Incorrect Username</h2><p>";
 			echo "<p>Incorrect Username</p>";
-		} 
+		}
 		else {
 			$obj = $resultIn->fetch_object();
 			// $tmp = MD5($password);
@@ -51,10 +29,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			if ($obj->Password_Hash == MD5($password)) {
 			// if ($obj->Password_Hash == hash("md5", $password, FALSE)) { // Make sure password was correct
 				echo "<p>Login Successful</p>";
-
-				
-
-				
 
 				$_SESSION["User_ID"] = $username;
 				// $_SESSION["User_ID"] = "testing";
@@ -90,45 +64,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			</div>
 
 			<div class="two-column">
-			<!-- <section class="todo">
-				<h2>Login</h2>
-				<div class="todo-body">
-					<ul class="todo-list">
-						<li>Username: <input type="text" name="userid" id="userid" title="username can be characters and numeric"></li>
-						<li>Password: <input type="text" name="password" id="password"></li>
-						<li><input type="submit" value="Login"></li>
-						<li><input type="reset" value="Clear Form"></li>
-					</ul>
-				</div>
-			</section> -->
-			<form method="post" id="addForm">
-				<fieldset>
-					<legend>User Info:</legend>
-					<p>
-						<label for="username">Username:</label>
-						<input type="text" class="required" name="username" id="username" title="username should be alphanumeric">
-					</p>
+				<section class="todo">
+					<form method="post" id="addForm">
+						<fieldset>
+						<legend>Login:</legend>
+							<div class="todo-body">
+								<ul class="todo-list">
+									<li>
+										<label for="username">Username:</label>
+										<input type="text" class="required" name="username" id="username" title="username should be alphanumeric">
+									</li>
+									<li>
+										<label for="password">Password:</label>
+										<input type="text" class="required" name="password" id="password">
+									</li>
+									<li><input type = "submit"  value = "Submit" /></li>
+									<li><input type = "reset"  value = "Clear Form" /></li>
+								</ul>
+							</div>
+						</fieldset>
+					</form>
+				</section>
 
-					<p>
-						<label for="password">Password:</label>
-						<input type="text" class="required" name="password" id="password">
-					</p>
-				</fieldset>
-
-				<p>
-					<input type = "submit"  value = "Submit" />
-					<input type = "reset"  value = "Clear Form" />
-				</p>
-			</form>
-
-
-			<section class="todo">
-				<h2><a href="create_account.php">Create Account</a></h2>
-			</section>
-		</div>
+				<section class="todo">
+					<h2><a href="create_account.php">Create Account</a></h2>
+				</section>
+			</div>
 
 	</div>
 </main>
 
 <?php include("footer.php"); ?>
-
