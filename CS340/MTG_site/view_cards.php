963131cd389b02e15fca12c188ba00638353a8d5
stribyw <stribyw@oregonstate.edu>
2018-06-14 16:04:37 -0700

    Adjusted the formatting of the file such that there are minimal echo statements and more PHP tags to mark when PHP code starts and stops. This is to fix the Your Cards table from showing last with CSS styling because the HTML tags are accessible outside the PHP. However, this did not work.

--- CS340/MTG_site/view_cards.php
+++ CS340/MTG_site/view_cards.php
@@ -1,32 +1,41 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <?php
-$currentpage="View Cards";
-include("header.php");
-include("db_connect.php");
+	$currentpage="View Cards";
+	include("header.php");
+	include("db_connect.php");
 
+	$user = $_SESSION["User_ID"];
+	$query = "SELECT * FROM User WHERE User_ID='$user'";
+	$result = mysqli_query($conn, $query);
+?>
 
-$user = $_SESSION["User_ID"];
-$query = "SELECT * FROM User WHERE User_ID='$user'";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) == 0) {
-	echo "<p>Please log in</p>";
-} else {
+<div class="two-row">
+	<div class="two-column">
+		<section class="todo">
+	<h2>Your Cards:</h2>
+	<input type="text" id="myInput" onkeyup="filterCardFunction()" placeholder="Search for names.." title="Type in a name">
 
+<?php
+	if (mysqli_num_rows($result) == 0) {
+?>
+	<!-- echo "<p>Please log in</p>"; -->
+	<p>Please log in</p>
+
+<?php
+	} else {
 	// Update counts if card added or removed
-	$modCard = $_GET['addcid'];
-	if ($modCard) {
-		$query = "UPDATE Collects SET Quantity=Quantity+1 WHERE Card_ID='$modCard' AND User_ID='$user'";
-		$conn->query($query);
-	}
-	$modCard = $_GET['rmcid'];
-	if ($modCard) {
-		$query = "UPDATE Collects SET Quantity=Quantity-1 WHERE Card_ID='$modCard' AND User_ID='$user'";
-		$conn->query($query);
-		mysqli_query($conn, "CALL Clear_Zeros");
-	}
-
-
+		$modCard = $_GET['addcid'];
+		if ($modCard) {
+			$query = "UPDATE Collects SET Quantity=Quantity+1 WHERE Card_ID='$modCard' AND User_ID='$user'";
+			$conn->query($query);
+		}
+		$modCard = $_GET['rmcid'];
+		if ($modCard) {
+			$query = "UPDATE Collects SET Quantity=Quantity-1 WHERE Card_ID='$modCard' AND User_ID='$user'";
+			$conn->query($query);
+			mysqli_query($conn, "CALL Clear_Zeros");
+		}
 
 	$query = "SELECT Card_ID, Name, Set_Name, Rarity, Quantity FROM Collects NATURAL JOIN Cards WHERE User_ID='$user'";
 	$result = mysqli_query($conn, $query);
@@ -36,9 +45,8 @@ if (mysqli_num_rows($result) == 0) {
 
 	// get number of columns in table
 	$fields_num = mysqli_num_fields($result);
-	echo "<h2>Your Cards:</h2>";
-	echo "<table id='t01' border='1'><tr>";
 
+	echo "<table id='t01' border='1'><tr>";
 	// printing table headers
 	for($i=0; $i<$fields_num; $i++) {
 		$field = mysqli_fetch_field($result);
@@ -46,130 +54,122 @@ if (mysqli_num_rows($result) == 0) {
 	}
 	echo "</tr>\n";
 	while($row = mysqli_fetch_row($result)) {
-		// echo "<tr onclick='cardClicked(this)'>";
 		echo "<tr>";
 		foreach($row as $cell)
 			echo "<td onclick='cardClicked(this)'>$cell</td>";
-		echo "<td onclick='addCardClicked(this)'><button>add1</button></td>";
-		echo "<td onclick='removeCardClicked(this)'><button>remove1</button></td>";
-		echo "</tr>\n";
+			echo "<td onclick='addCardClicked(this)'><button>add1</button></td>";
+			echo "<td onclick='removeCardClicked(this)'><button>remove1</button></td>";
+			echo "</tr>\n";
 	}
+?>
+
+</section>
+</div>
 
 
-
+	<div class="two-column">
+		<section class="todo">
+<?php
 	$selCard = $_GET['cid'];
 	if ($selCard) {
 		// Card Info
 		$query = "SELECT Name, Rarity, Set_Name FROM Cards WHERE Card_ID='$selCard'";
 		$result = mysqli_query($conn, $query);
 		if (!$result) { die("Query to show fields from table failed");}
-		echo "<p>Card Info: ";
-		foreach(mysqli_fetch_row($result) as $cell)
-			echo "$cell, ";
-		echo "</p>";
-
-		// Card Count
-		$query = "SELECT COUNT(*) FROM `Collects` WHERE Card_ID='$selCard'";
-		$result = mysqli_query($conn, $query);
-		if (!$result) { die("Query to show fields from table failed");}
-		echo "<p>Card Count (of everyone): ";
-		foreach(mysqli_fetch_row($result) as $cell)
-			echo "$cell ";
-		echo "</p>";
-
-		// Number of this user's decks its in
-		$query = "SELECT COUNT(*) FROM Contains NATURAL JOIN Decks NATURAL JOIN User WHERE Card_ID='$selCard' AND User_ID = '$user'";		
-		$result = mysqli_query($conn, $query);
-		// echo $result;
-		if (!$result) { die("Query to show fields from table failed2");}
-		echo "<p>Number of your decks its in: ";
-		foreach(mysqli_fetch_row($result) as $cell)
-			echo "$cell ";
-		echo "</p>";
-
-		// Discussions its in
-		$query = "SELECT COUNT(Card_ID) FROM Discussions WHERE Card_ID='$selCard'";
-		$result = mysqli_query($conn, $query);
-		if (!$result) { die("Query to show fields from table failed3");}
-		echo "<p>Discussions its in: ";
-		foreach(mysqli_fetch_row($result) as $cell)
-			echo "$cell ";
-		echo "</p>";
-
-		// Trades its in
-		$query = "SELECT COUNT(Card_ID) FROM Trade_Have WHERE Card_ID='$selCard'";
-
-		$result = mysqli_query($conn, $query);
-		if (!$result) { die("Query to show fields from table failed4");}
-		echo "<p>Trades its offered in: ";
-		foreach(mysqli_fetch_row($result) as $cell)
-			echo "$cell ";
-		echo "</p>";
-	}
-
-
-
-	// echo "<div class='two-column'>";
-	// echo "<h2 id=selCard>Current Card Name:</h2>";
-	// echo "<div class='todo-body'>";
-	// echo "<div class='todo-list'>";
-	// echo "<p>Test</p>";
-	// echo "</div>";
-	// echo "</div>";
-	// echo "</div>";
-	// echo "<div>";
-	// echo "<div class='two-column'>";
-	// 	echo "<section class='todo'>";
-	// 		echo "<h2>Some header:</h2>";
-	// 		echo "<div class='todo-body'>";
-	// 			echo "<div class='todo-list'>";
-	// 				echo "<p>Test</p>";
-	// 			echo "</ul>";
-	// 		echo "</div>";
-	// 	echo "</section>";
-	// echo "</div>";
-
-	// echo "<div class='two-column'>";
-	// 	echo "<section class='todo'>";
-
-	// 	echo "</section>";
-
-	// 	echo "<section class='todo'>";
-
-	// 	echo "</section>";
-	// echo "</div>";
-	// echo "</div>";
-}
-mysqli_free_result($result);
-mysqli_close($conn);
 ?>
 
 
-<main>
-	<input type="text" id="myInput" onkeyup="filterCardFunction()" placeholder="Search for names.." title="Type in a name">
+	<!-- Start Display of Card Info for selected card -->
+	<h3>Card Info:</h3>
+	<p>
 
+<?php
+	foreach(mysqli_fetch_row($result) as $cell)
+		echo "$cell, ";
+?>
 
+	</p>
 
-<!-- 
-	<div class="two-column">
-		<h2 id=selCard>Some header:</h2>
-		<div class="todo-body">
-			<div class="todo-list">
-				<p>Test</p>
-			</ul>
-		</div>
-	</div> -->
-<!-- 
-	<div class="two-column">
-		<section class="todo">
-			
-		</section>
+<?php
+	// Card Count
+	$query = "SELECT COUNT(*) FROM `Collects` WHERE Card_ID='$selCard'";
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+?>
 
-		<section class="todo">
-			
-		</section>
-	</div> -->
+	<p>Card Count (of everyone):
 
-</main>
+<?php
+	foreach(mysqli_fetch_row($result) as $cell)
+		echo "$cell ";
+?>
+
+	</p>
+
+<?php
+	// Number of this user's decks its in
+	$query = "SELECT COUNT(*) FROM Contains NATURAL JOIN Decks NATURAL JOIN User WHERE Card_ID='$selCard' AND User_ID = '$user'";
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		die("Query to show fields from table failed2");
+	}
+
+?>
+
+  <p>Number of your decks its in:
+
+<?php
+	foreach(mysqli_fetch_row($result) as $cell)
+		echo "$cell ";
+?>
+
+	</p>
+
+<?php
+	// Discussions its in
+	$query = "SELECT COUNT(Card_ID) FROM Discussions WHERE Card_ID='$selCard'";
+	$result = mysqli_query($conn, $query);
+	if (!$result) { die("Query to show fields from table failed3");}
+?>
+
+	<p>Discussions its in:
+
+<?php
+	foreach(mysqli_fetch_row($result) as $cell)
+		echo "$cell ";
+?>
+
+</p>
+
+<?php
+	// Trades its in
+	$query = "SELECT COUNT(Card_ID) FROM Trade_Have WHERE Card_ID='$selCard'";
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		die("Query to show fields from table failed4");
+	}
+?>
+
+	<p>Trades its offered in:
+
+<?php
+	foreach(mysqli_fetch_row($result) as $cell)
+		echo "$cell ";
+?>
+
+	</p>
+
+</section>
+</div>
+</div>
+
+<?php
+	}
+}
+
+mysqli_close($conn);
+?>
 
 <?php include("footer.php"); ?>
